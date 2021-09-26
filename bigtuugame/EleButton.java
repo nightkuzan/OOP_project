e1bf6d69f82cf902fa3971e23bdd50f3fc78b7e5
unknown <aekkarit12@gmail.com>
2021-09-26 15:38:48 +0700

    fix serial

--- bigtuugame/EleButton.java
+++ bigtuugame/EleButton.java
@@ -4,7 +4,6 @@ package bigtuugame;
 import java.awt.Color;
 import javax.swing.JButton;
 public class EleButton extends JButton {
-    private static final long serialVersionUID = 1L;
 
 		public EleButton(String title,int size,int x,int y,int width,int height) {
 			super(title);
--- bigtuugame/EleLabel.java
+++ bigtuugame/EleLabel.java
@@ -2,7 +2,6 @@ package bigtuugame;
 
 import javax.swing.JLabel;
 public class EleLabel extends JLabel {
-    private static final long serialVersionUID = 1L;
 		
 		public EleLabel(String title,int size,int x,int y,int w,int h) {
 			 super(title);
